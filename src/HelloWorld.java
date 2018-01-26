ed019f54a9aa9055b3a107290f8a043a41117d87
MasterMatthew <Mastamats@yahoo.com>
2018-01-26 15:43:37 -0500

    origin

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+import com.base.engine.core.CoreEngine;
+
+import math.Vector3f;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		CoreEngine ce = new CoreEngine();
+		ce.start();
+	}
+}
